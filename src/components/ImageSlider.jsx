003cce64b4bcd9964a7cc81bbcfea64a5275d6e7
fox <mernweb@gmail.com>
2025-04-07 00:48:46 +0530

    mouse hover issue

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,20 +5,34 @@ import * as THREE from 'three'
 import VertextShader from '../shaders/slider/vertex.glsl'
 import FragmentShader from '../shaders/slider/fragment.glsl'
 
+const texutures = ['1.jpg', '2.jpg','3.jpg','4.jpg','5.jpg']
+
 function ImageSlider({ width = 4, height = 3, aspect = 0.75 }) {
 
+    const mesh = React.useRef()
+    const prevTexture = useTexture('/img/2.jpg')
     const texture1 = useTexture('/img/1.jpg')
     const viewport = useThree(s=>s.viewport)
     let ratio =  viewport.height * aspect / height
     if(viewport.width < viewport.height) ratio =  viewport.width * aspect / width
 
+
+    const mouseHandler = (e) => {
+      console.log('dfs');
+      
+        const x = e.clientX / window.innerWidth
+        const y = e.clientY / window.innerHeight
+        mesh.current.material.uniforms.uMouse.value = new THREE.Vector2(x, y)
+    }
+
   return (
-    <mesh>
-      <planeGeometry args={[width * ratio, height * ratio]} />
+    <mesh onPointerMove={mouseHandler} ref={mesh}>
+      <planeGeometry args={[width * ratio, height * ratio, 32, 32]} />
       <shaderMaterial 
         vertexShader={VertextShader} 
         fragmentShader={FragmentShader} 
         uniforms={{
+            uMouse: null,
             uTexture1 : new THREE.Uniform(texture1),
         }}
       />
--- src/shaders/slider/vertex.glsl
+++ src/shaders/slider/vertex.glsl
@@ -1,8 +1,14 @@
 varying vec2 vUv;
+uniform vec2 uMouse;
 
 void main(){
 
     vUv = uv;
+    vec2 center = uv - 0.5;
+    // float dis = distance(center, uMouse);
 
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+    // newPosition.z = dis * 100.0;
+
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(newPosition, 1.0);
 }
